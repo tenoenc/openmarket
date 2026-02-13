8ac1969b3e65e3a46003121bfe6e2cc0aeac2071
tenoenc <tenoenc@gmail.com>
2026-02-13 14:36:03 +0900

    chore(api): 메인 애플리케이션 클래스 생성 (빌드 가능) (#1)

---
+++ api-server/src/main/java/com/teno/openmarket/api/OpenMarketApplication.java
@@ -0,0 +1,11 @@
+package com.teno.openmarket.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenMarketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenMarketApplication.class, args);
+    }
+}
