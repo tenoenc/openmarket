e1e05691ec6da3fccf7fb5899bf2570eb085a497
tenoenc <tenoenc@gmail.com>
2026-02-15 04:01:48 +0900

    chore(common): Stylecheck 테스트 통과를 위한 멀티 모듈 관련 코드 제거 (#1)

--- user/src/main/java/com/teno/openmarket/user/domain/UserDummy.java
+++
@@ -1,9 +0,0 @@
-package com.teno.openmarket.user.domain;
-
-//import com.teno.order.domain.OrderDummy;
-
-public class UserDummy {
-    public void test() {
-//        OrderDummy order = new OrderDummy();
-    }
-}
