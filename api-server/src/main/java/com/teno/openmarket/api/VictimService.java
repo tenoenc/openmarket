8cdc66d0e6cdb42338be6af36ed04eb9969b2117
tenoenc <tenoenc@gmail.com>
2026-02-15 03:15:48 +0900

    test(common): Jacoco Report 생성 및 커버리지 검증 테스트 (#4)

---
+++ api-server/src/main/java/com/teno/openmarket/api/VictimService.java
@@ -0,0 +1,12 @@
+package com.teno.openmarket.api;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VictimService {
+
+    // 이 메서드를 테스트하지 않으면 커버리지는 0%가 됨
+    public String logic() {
+        return "I am a victim.";
+    }
+}
---
+++ api-server/src/test/java/com/teno/openmarket/api/HelloTest.java
@@ -0,0 +1,11 @@
+package com.teno.openmarket.api;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+
+    @Test
+    void report_Generation_Test() {
+        System.out.println("리포트를 만들기 위한 빈 테스트입니다.");
+    }
+}
