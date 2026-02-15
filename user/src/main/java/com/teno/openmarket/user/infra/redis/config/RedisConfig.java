c517d9dd0dacfb245a887a39c7ff8bf59fc3b9ce
tenoenc <tenoenc@gmail.com>
2026-02-15 23:13:40 +0900

    style(user): Modular를 위해 config 패키지를 redis 패키지 아래로 이동 (#12)

--- user/src/main/java/com/teno/openmarket/user/infra/redis/config/RedisConfig.java
+++ user/src/main/java/com/teno/openmarket/user/infra/redis/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.teno.openmarket.user.infra.config;
+package com.teno.openmarket.user.infra.redis.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
