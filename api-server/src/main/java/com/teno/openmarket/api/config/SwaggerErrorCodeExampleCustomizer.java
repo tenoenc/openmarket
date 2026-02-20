13888bb54fef169fd057eb153e7a11b2877195e9
tenoenc <tenoenc@gmail.com>
2026-02-20 23:40:39 +0900

    style(api): Style Check 에러 해결 (AvoidStarImport) (#82)

--- api-server/src/main/java/com/teno/openmarket/api/config/SwaggerErrorCodeExampleCustomizer.java
+++ api-server/src/main/java/com/teno/openmarket/api/config/SwaggerErrorCodeExampleCustomizer.java
@@ -18,7 +18,11 @@ import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
